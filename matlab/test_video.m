edcbc9670eb029798cc40c6d3915bdd460e5d7e3
LJacki <LJacki@163.com>
2021-03-07 11:25:27 +0800

    增加字模

--- matlab/test_video.m
+++ matlab/test_video.m
@@ -1,25 +1,24 @@
 % close all;
 clear;
 
-j=0:1:64;
-i=0:1:128;
-[x,y] = meshgrid(i,j);
+%% parameter declariton
+hcnt = 16;
+vcnt = 8;
+%% meshgrid
+map_v = 0:1:vcnt;
+map_h = 0:1:hcnt;
+[x,y] = meshgrid(map_v,map_h);
 set(gca, 'color', [0 0 0]);
 plot(x,y,'w')
 hold on
 plot(x',y','w')
 
- for i = 0:1:128
-     for j = 0:1:64
-         draw_pixel(i, j, 'g');
+%% draw pattern only one color one pixel
+ for h = 0:1:hcnt-1
+     for v = 0:1:vcnt-1
+         draw_pixel(v, hcnt-1-h, [0.2, 0.2, 0.2]);
          pause(0.001)
      end
- end
-
- for i = 42:1:80
-     for j = 0:1:15
-         draw_pixel(i, j, 'r');
-         pause(0.001)
-     end
+%      pause(0.5)
  end
  
